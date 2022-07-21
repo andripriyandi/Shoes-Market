32cf7dcd41639c96c8d2a67a8fe653fdbca0230e
Andri Priyandi <andripriyandi@gmail.com>
2022-07-21 16:26:44 +0700

    [FEAT]: Checkout page address details

--- lib/pages/checkout/checkout_page.dart
+++ lib/pages/checkout/checkout_page.dart
@@ -42,6 +42,90 @@ class CheckoutPage extends StatelessWidget {
                 const CheckoutCard()
               ],
             ),
+          ),
+
+          // NOTE : ADDRESS DETAIL
+          Container(
+            margin: EdgeInsets.only(
+              top: defaultMargin,
+            ),
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: backgroundColor4,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Address Details',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16.sp,
+                    fontWeight: medium,
+                  ),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        Image.asset(
+                          'assets/icons/icon_store_location.png',
+                          width: 40.w,
+                        ),
+                        Image.asset(
+                          'assets/icons/icon_line.png',
+                          height: 30.w,
+                        ),
+                        Image.asset(
+                          'assets/icons/icon_your_address.png',
+                          width: 40.w,
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 12,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Store Location',
+                          style: secondaryTextStyle.copyWith(
+                            fontSize: 12.sp,
+                            fontWeight: light,
+                          ),
+                        ),
+                        Text(
+                          'Adidas Core',
+                          style: primaryTextStyle.copyWith(
+                            fontWeight: medium,
+                          ),
+                        ),
+                        SizedBox(
+                          height: defaultMargin,
+                        ),
+                        Text(
+                          'Your Address',
+                          style: secondaryTextStyle.copyWith(
+                            fontSize: 12.sp,
+                            fontWeight: light,
+                          ),
+                        ),
+                        Text(
+                          'Marsemoon',
+                          style: primaryTextStyle.copyWith(
+                            fontWeight: medium,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           )
         ],
       );
